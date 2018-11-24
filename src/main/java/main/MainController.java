6ea78dd1328da6ceea6ae8522a5e02b064ff6263
Omar Enrique Castro <oecastrom@gmail.com>
2018-11-24 16:08:50 -0600

    initial commit

---
+++ src/main/java/main/MainController.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Omar E. Castro
+ * Date: 11/24/2018
+ */
+
+@SpringBootApplication
+public class MainController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainController.class, args);
+    }
+}
